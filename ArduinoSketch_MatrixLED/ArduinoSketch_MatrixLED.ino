7cdb91eaadb7dfd5488cc68de264ec9952019ed4
Hong Quach <hong.t.quach@gmail.com>
2015-11-25 10:13:10 -0800

    Initial check in of the completed working code of an 8x32 Matrix LED

---
+++ ArduinoSketch_MatrixLED/ArduinoSketch_MatrixLED.ino
@@ -0,0 +1,167 @@
+/**
+ * Copyright (c) 2015 Hong Quach and Norah Alballa
+ *
+ * This source file is licensed under the "MIT License." Please see the LICENSE
+ * in this distribution for license terms.
+ *
+ * Matrix rendering controlled by serial
+ * Send each frame through the serial port (64 bytes buffer size)
+ * marked with the startChar and end with the endChar.
+ *
+ */
+
+String inputString = "";         // a string to hold incoming data
+boolean stringComplete = false;  // whether the string is complete
+char startChar = '^';            // char to indicate the start of matrix data
+char endChar = '$';              // char to indicate the end of the matrix data
+boolean dataStarted = false;     // signal that matrix data is comming in
+
+// Initial frame
+String matrixData = "ff00ff0000ff00ffff00ff0000ff00ffff00ff0000ff00ffff00ff0000ff00ff";
+String newMatrixData = "";
+
+boolean enableDebug = false;
+int matrixWidth = 32;
+int matrixHeight = 8;
+int serialSpeed = 9600;
+
+//Pin connected to latch pin #12 (ST_CP) of 74HC595
+const int latchPin = 12;
+//Pin connected to clock pin #11 (SH_CP) of 74HC595
+const int clockPin = 11;
+//Pin connected to Data pin #14 (DS) of 74HC595
+const int dataPin = 4;
+//Pin connect to LED for visual feedback for debug purpose
+const int ledPin = 13;
+
+/**
+ * Arduino init function
+ */
+void setup() {
+  //set pins to output because they are addressed in the main loop
+  pinMode(latchPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+  Serial.begin(serialSpeed);
+  Serial.print("Starting Listing on Serial Port.  ");
+  Serial.println("Use '^' to start a new data and '$' to terminate.");
+  // reserve 200 bytes for strings:
+  matrixData.reserve(200);
+  newMatrixData.reserve(200);
+  //TODO:  Send out the startChar, endChar, matrix Width and Height to the Host
+}
+
+/**
+ * Recieve chars from serial port
+ */
+void getSerialInput() {
+  for (int i = 0; i < 66; i++) {
+    if (Serial.available()) {
+      char inChar = (char)Serial.read();
+      if (inChar == startChar && !dataStarted) {
+        if (enableDebug) {
+          digitalWrite(ledPin, true);
+          Serial.println("Start char detected");
+        }
+        newMatrixData = "";
+        dataStarted = true;
+      }
+      else if (inChar == endChar && dataStarted) {
+        if (enableDebug) {
+          Serial.println("End   char detected");
+        }
+        if (newMatrixData == "^enableDebug^") {
+          enableDebug = true;
+        }
+        else if (newMatrixData == "^disableDebug^") {
+          enableDebug = false;
+        }
+        else {
+          matrixData = newMatrixData;
+          dataStarted = false;
+        }
+        if (enableDebug) {
+          digitalWrite(ledPin, false);
+          Serial.print("New Frame:  ");
+          Serial.println(matrixData);
+        }
+      }
+      else if (dataStarted) {
+        newMatrixData += inChar;
+      }
+    }
+    else {
+      break;
+    }
+  }
+}
+
+/**
+ * The 'main' of Arduino
+ */
+void loop() {
+  getSerialInput();
+  render(matrixData, 1);
+}
+
+/**
+ * Render each frame
+ */
+void render(String frame, int holdCycle) {
+  int registerCount = 5;
+  int layerCount = 8;
+  byte registers[registerCount];
+  for (int h = 0; h < holdCycle; h++) {
+    unsigned layerBit = 1;
+    for (int layer = 0; layer < layerCount; layer++) {
+      // Lines up the bits to be shipped to the register
+      int subFrameIndex = layer * layerCount;
+      registers[0] = byte(layerBit);
+      for (int r = 1; r < registerCount; r++)
+      {
+        registers[r] = hexStringToByte(frame.substring(subFrameIndex + ((r - 1) * 2), subFrameIndex + (r * 2)));
+      }
+      shiftOutToRegisters(registerCount, registers);
+
+      //Next layer
+      layerBit = (layerBit << 1);
+    }
+  }
+}
+
+/**
+ * shifting one byte per register (74HC595) wired in series.
+ */
+void shiftOutToRegisters(int registerCount, byte* registers) {
+  // turn off the output so the pins don't light up
+  // while you're shifting bits:
+  digitalWrite(latchPin, LOW);
+  for (int i = 0; i < registerCount; i++) {
+    shiftOut(dataPin, clockPin, MSBFIRST, registers[i]);
+  }
+  // turn on the output so the LEDs can light up:
+  digitalWrite(latchPin, HIGH);
+}
+
+/**
+ * Convert a string in HEX to a byte
+ */
+byte hexStringToByte(String hexString) {
+  byte temp = 0;
+  int input_len = hexString.length();
+  hexString.toUpperCase();
+  for (int i = 0; i < input_len; i++) {
+    int int_of_char = int(hexString[i]);
+    int int_of_0 = int('0');
+    int int_of_A = int('A');
+    int char_value = 0;
+    if (int_of_char >= int_of_0 && int_of_char <= int_of_0 + 9) {
+      char_value = int_of_char - int_of_0;
+    } else if (int_of_char >= int_of_A && int_of_char <= int_of_A + 5) {
+      char_value = int_of_char - int_of_A + 10;
+    }
+    temp |= (char_value << ((input_len - 1 - i) * 4));
+  }
+  return temp;
+}
+
---
+++ LEDDisplay.hs
@@ -0,0 +1,153 @@
+module LEDDisplay  where
+------------------------------------------------------------------------
+-- Copyright (c) 2015 Hong Quach and Norah Alballa
+--
+-- This source file is licensed under the "MIT License." Please see the LICENSE
+-- in this distribution for license terms.
+--
+-- Control an 8x8 Matrix of LED. User will enter a string from the prompt
+-- to have it scroll on the 8x8 Matrix of LED.
+--
+-- The user would submit
+-- a string through the GHCi terminal on the computer and it will be scroll
+-- across on the LED matrix. Depending on the scope of the project, we can
+-- also look into adding more animation of the text on the display. This
+-- project can be extended to drive LED Cube and other wide I/O projects
+-- (e.g. more LED 8xN) with little to no modification. The string to be
+-- display on the matrix can also come from another source, such as an
+-- email notification or system status.
+------------------------------------------------------------------------
+
+import Control.Monad        (forever)
+import Control.Monad.Trans  (liftIO)
+import Control.Concurrent   (threadDelay)
+import System.IO            (FilePath)
+import System.Hardware.Serialport
+import qualified Data.ByteString.Char8 as B
+import Numeric  -- Convert string to Hex
+import Data.Time
+
+import CharsLookup
+
+
+-- Constants for Matrix LED frame dimensions, delay between frames, and text scroll speed
+frameWidth :: Int
+frameWidth = 8 * 4
+frameHeight :: Int
+frameHeight = 8
+frameDelay :: Int
+frameDelay = 10
+scrollDelayMS :: Int
+scrollDelayMS = 10000
+scrollColumnsCount :: Int
+scrollColumnsCount = 1
+
+serialPort :: String
+serialPort = "COM13"
+serialSpeed :: CommSpeed
+serialSpeed = CS9600
+frameStartChar :: Char
+frameStartChar = '^'
+frameEndChar :: Char
+frameEndChar = '$'
+
+
+-- The bit value that would indicate the LED is on or off.
+-- The actual value is due to the indicate the polarity of the LED
+lightOn :: Bool
+lightOn = False
+lightOff :: Bool
+lightOff = True
+
+-- The main program that loop forever on getLine and render it on the Matrix LED
+main :: IO Int
+main = do
+    withSerial serialPort defaultSerialSettings { commSpeed = serialSpeed } runForever
+
+-- Prompt for string and render it
+runForever :: SerialPort -> IO Int
+runForever hw = do
+    liftIO $ putStrLn "Initialize"
+    threadDelay 3000000
+    liftIO $ putStrLn "StartUp Logo"
+    renderMatrix 1 hw $ padMatrix (toBoolGrid (specialStrings "Haskell Logo"))
+    threadDelay 2000000
+    liftIO $ putStr "Ready"
+    forever $ do
+        liftIO $ putStr "Text to display: "
+        msg <- liftIO getLine
+        -- TODO: Add exception handling for quiting (Ctrl C or msg == ":q")
+        -- See tryJust https://hackage.haskell.org/package/base-4.8.1.0/docs/Control-Exception.html#v:tryJust
+        renderMatrix scrollColumnsCount hw $ padMatrix (stringToMatrix msg)
+
+-- Render the matrix on the Matrix LED
+renderMatrix :: Int -> SerialPort -> [[Bool]] -> IO ()
+renderMatrix scrollSpacing hw matrix = do
+    mapM_ (scrollFrame hw matrix) [0..(((length (matrix !! 0)) - frameWidth - 1) `div` scrollSpacing)]
+    threadDelay 1
+    where
+        scrollFrame s m n= do
+            renderFrameOntoHW s (matrixToFrameGrid ((matrixToFrames scrollSpacing m) !! n))
+            threadDelay scrollDelayMS
+
+-- Each matrix has many frames to animate scrolling
+matrixToFrames :: Int -> [[Bool]] -> [[[Bool]]]
+matrixToFrames scrollSpacing bss = [[drop (scrollSpacing * x) bs | bs <- bss] | x <- [0..((length (bss !! 0)) - frameWidth)]]
+
+-- The dataToRender is a string of hex encoded bits to send for render.
+renderFrameOntoHW :: SerialPort -> String -> IO Int
+renderFrameOntoHW hw dataToRender = do
+    liftIO $ print dataToRender
+    send hw $ B.pack [frameStartChar]
+    mapM_ (send hw) [B.pack [c] | c <- dataToRender]
+    send hw $ B.pack [frameEndChar]
+
+-- Convert a string to the matrix representation.
+-- Note add a blank column in between each char.
+stringToMatrix :: String -> [[Bool]]
+stringToMatrix [] = [[]]
+stringToMatrix (c:[]) =  letterToMatrix c
+stringToMatrix (c:cs) =  concatMatrix (letterToMatrix c) (stringToMatrix cs)
+
+-- concat two nested lists into one nested list
+concatMatrix :: [[Bool]] -> [[Bool]] -> [[Bool]]
+concatMatrix [[]]    [[]]  = [[]]
+concatMatrix [[]]     ts   = ts
+concatMatrix   cs    [[]]  = cs
+concatMatrix (c:[]) (t:[]) = [ (c ++ lightOff:t) ]
+concatMatrix (c:[]) (t:ts) = [ (c ++ lightOff:t) ] ++ ts
+concatMatrix (c:cs) (t:[]) =  cs ++ [ (c ++ lightOff:t) ]
+concatMatrix (c:cs) (t:ts) = (c ++ lightOff:t) :  concatMatrix cs ts
+
+-- Truncate a matrix to fit in the frame frameWidth x frameHeight.
+matrixToFrameGrid :: [[Bool]] -> String
+matrixToFrameGrid matrix = concat [fromIntToHexString (take frameWidth row) | row <- matrix]
+
+-- Convert a String of Bool into a String of Hex  11110000 -> 0xF0
+fromIntToHexString :: [Bool] -> String
+fromIntToHexString [] = ""
+fromIntToHexString bs = showHex (fromBoolToInt(take 4 bs)) (fromIntToHexString (drop 4 bs)) where
+    fromBoolToInt :: [Bool] -> Int
+    fromBoolToInt bs = fromBoolToIntLSB (reverse bs) where
+        fromBoolToIntLSB []     = 0
+        fromBoolToIntLSB (True:ds) = 1 + (2 * fromBoolToIntLSB ds)
+        fromBoolToIntLSB (False:ds) = 2 * fromBoolToIntLSB ds
+
+-- Shift the matrix in different directions:  up, down, left, or right ('u', 'd', 'l' or 'r').
+shiftMatrix :: Char -> [[Int]] -> [[Int]]
+shiftMatrix = undefined
+
+
+-- Some test patterns
+testPatterns = [[(c `mod` (r+1)) /= 0 | c <- [0..(frameWidth - 1)]] | r <- [0..(frameHeight - 1)]]
+testPatternsInvert = [[(c `mod` (r+1)) == 0 | c <- [0..(frameWidth - 1)]] | r <- [0..(frameHeight - 1)]]
+matrixAllOn = [[True | c <- [0..(frameWidth - 1)]] | r <- [0..(frameHeight - 1)]]
+matrixAllOff = [[False | c <- [0..(frameWidth - 1)]] | r <- [0..(frameHeight - 1)]]
+
+-- An empty Frame to show all blank
+emptyFrame :: [[Bool]]
+emptyFrame = [[lightOff | c <- [1..frameWidth]] | r <- [1..frameHeight]]
+
+padMatrix :: [[Bool]] -> [[Bool]]
+padMatrix m = concatMatrix (concatMatrix emptyFrame m) emptyFrame
+
